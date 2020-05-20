1378ad5f3a9f5cc1f5e891dc329803a8ecc39264
Carol Chung <cch5ng@gmail.com>
2020-05-20 09:30:26 -0700

    add updated_on date for simpler sorting of latest convos

--- server/database/models/conversation.js
+++ server/database/models/conversation.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/* the updated_on date should be included whenever 
+    a message gets added to a conversation so display 
+    sort order can be latest to oldest
+*/
 var conversationSchema = new Schema({
     users: {
         type: [mongoose.Types.ObjectId],
@@ -18,7 +22,8 @@ var conversationSchema = new Schema({
         img_url: String,
         created_on: { type: Date, default: Date.now }
     }],
-    created_on: { type: Date, default: Date.now }
+    created_on: { type: Date, default: Date.now },
+    updated_on: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Conversation', conversationSchema, 'conversations');
