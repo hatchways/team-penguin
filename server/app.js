14418d5d41ec4698fc92f06611c2c938db184182
Carol Chung <cch5ng@gmail.com>
2020-05-20 09:13:22 -0700

    add model for invitation (#26)
    * add model for invitation
    
    * add init approved value to aid in query received invites
    
    * update reference object id in another collection
    
    * add initial route for get received invites, #15 ticket
    
    * update the ObjectId type
    
    * removed /invitations at the end of route (prepended with)

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
+const invitationRouter = require("./routes/invitation");
 
 const { json, urlencoded } = express;
 
@@ -26,6 +27,7 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+app.use("/invitations", invitationRouter);
 
 //connect to mongodb
 mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true})
