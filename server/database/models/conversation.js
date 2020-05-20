08c71f1d983cf64da8005b938b18780edc597ddc
Carol Chung <cch5ng@gmail.com>
2020-05-19 22:13:20 -0700

    rename file

---
+++ server/database/models/conversation.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var conversationSchema = new Schema({
+    users: {
+        type: [mongoose.Types.ObjectId],
+        ref: 'users',
+        required: true
+    },
+    messages: [{
+        author_id: {
+            type: mongoose.Types.ObjectId,
+            ref: 'users',
+            required: true
+        },
+        body: String,
+        language: String,
+        img_url: String,
+        created_on: { type: Date, default: Date.now }
+    }],
+    created_on: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Conversation', conversationSchema, 'conversations');
