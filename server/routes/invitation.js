4c20d8dfc4704e49a9eebeec685d2bf7bd100207
Carol Chung <cch5ng@gmail.com>
2020-05-20 10:55:54 -0700

    stub out init route for user"s contacts; need to fix the query to aggreg with user data

--- server/routes/invitation.js
+++ server/routes/invitation.js
@@ -4,6 +4,7 @@ const passport = require("passport");
 const Invitation = require("../database/models/invitation");
 const router = express.Router();
 
+// Returns pending invitations that were sent to the given user
 router.get("/user/:id",
     passport.authenticate('jwt', { session: false }),
     function(req, res, next) {
@@ -12,6 +13,29 @@ router.get("/user/:id",
 
         Invitation.find({"to_user": new objId(id), "approved": false, "rejected": {$ne: true}})
             .sort({createdOn: 1})
+            .exec(function (err, invitations) {
+                if (err) {
+                    return handleError(err);
+                }
+                if (invitations && invitations.length) {
+                    res.json({ type: "success", invitations})
+                } else {
+                    res.json({ type: "success", message: "There are no pending invitations"})
+                }
+            }
+        )
+    }
+);
+
+// Returns contacts that the current user accepted invitations from
+// FE should display email and probably needs the _id in order to send messages
+router.get("/user/:id/contacts",
+    passport.authenticate('jwt', { session: false }),
+    function(req, res, next) {
+        const {id} = req.params;
+        var objId = mongoose.Types.ObjectId;
+
+        Invitation.find({"to_user": new objId(id), "approved": true })
             .exec(function (err, invitations) {
                 if (err) {
                     return handleError(err);
