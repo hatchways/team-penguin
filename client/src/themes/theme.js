67660b5963d94d6dc32b2c9f312e1c498dc5c20f
Carol Chung <cch5ng@gmail.com>
2020-05-21 16:44:43 -0700

    fix warning with default font size val

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -3,9 +3,10 @@ import { createMuiTheme } from "@material-ui/core";
 export const theme = createMuiTheme({
   typography: {
     fontFamily: "Open Sans",
-    fontSize: 12,
+    fontSize: 14,
     h1: {
       // could customize the h1 variant as well
+      fontWeight: 600
     }
   },
   palette: {
