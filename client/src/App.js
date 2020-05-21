c089f352aad4dd6b8e8545438b6b1c1bb32c3b85
Lakshmi Padala <Lakshmippadala@gmail.com>
2020-05-21 14:44:02 -0500

    Please review Chat,Contacts.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
-
-import { theme } from "./themes/theme";
+import Home from "./components/Home/Home";
+import {Theme} from "./themes/Theme";
 import LandingPage from "./pages/Landing";
 
-import "./App.css";
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
+    <div>
+    <MuiThemeProvider Theme={Theme}>
       <BrowserRouter>
+        <Route path="/Home" component={Home} />
         <Route path="/" component={LandingPage} />
       </BrowserRouter>
     </MuiThemeProvider>
+    </div>
   );
 }
 
