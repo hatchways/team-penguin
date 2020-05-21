6bb84ac61787d2dddfea32440ad05516ec477cda
Rajat Bansal <rjtbansal@gmail.com>
2020-05-20 22:22:18 -0400

    added user login validator and updated user registration validator

--- server/controllers/userloginValidator.js
+++ server/controllers/userloginValidator.js
@@ -1,15 +1,13 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-const userRegistrationValidator = formData => {
+const userLoginValidator = formData => {
 
   const validationErrors = {};
 
-  //validator wont work without converting empty fields to strings
   formData.email = !isEmpty(formData.email) ? formData.email : '';
   formData.password = !isEmpty(formData.password) ? formData.password : '';
 
-  //validations check
   if(Validator.isEmpty(formData.email)) {
     validationErrors.email = 'Email is a required field';
   }
@@ -20,18 +18,10 @@ const userRegistrationValidator = formData => {
   if(Validator.isEmpty(formData.password)) {
     validationErrors.password = 'Password is a required field';
   }
-  else if(!Validator.isLength(formData.password, {min: 6})) {
-    validationErrors.password = 'Password should have atleast 6 characters';
-  }
 
-  if(!Validator.equals(formData.password, formData.confirmPassword)) {
-    validationErrors.confirmPassword = 'Confirm Password not matching with Password';
-  }
-  
   return {
     validationErrors,
     isRegistrationValid: isEmpty(validationErrors)
   }
-}
 
-module.exports = userRegistrationValidator;
+}
