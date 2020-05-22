e0b5e6a78a118d6531383849c99e0818e4e0ee1d
Carol Chung <cch5ng@gmail.com>
2020-05-22 15:20:08 -0700

    push grid definition up one level in component hierarchy

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
-
-import Sidebar from './Sidebar/Sidebar';
-import Chat from './Chat/Chat';
+import Grid from '@material-ui/core/Grid';
+import Sidebar from '../Sidebar/Sidebar';
+import Chat from '../Chat/Chat';
 
 class Home extends Component {
     render() {
         return (
-    
-         <div>
-                <Sidebar/>
-                <Chat/>
+            <div>
+                <Grid container spacing={3}>
+                <Grid
+                    item xs={4}
+                    //sm={3}
+                    direction='row'
+                    //className={props.classes.root}
+                >
+                    <Sidebar/>
+                </Grid>
+
+                <Grid
+                    item xs={8}
+                    direction='row' >
+                    <Chat/>
+                </Grid>
+
+                </Grid>
             </div>
         )
     }
 }
-
 export default Home
-
