17cd5683076a410532aa0dd0242ebb2871b5a5de
Carol Chung <cch5ng@gmail.com>
2020-05-21 17:48:45 -0700

    Get contacts 17 (#28)
    * stub out init route for user"s contacts; need to fix the query to aggreg with user data
    
    * stub out init route for user"s contacts; need to fix the query to aggreg with user data
    
    * attempt at aggregate function
    
    * stub out init route for user"s contacts; need to fix the query to aggreg with user data
    
    * attempt at aggregate function
    
    * modify the get endpoint for user"s contacts; removed aggregate
    
    * revert to search using aggregate function, tested
    
    * moved models to model folder
    
    * added the create invitation request from #19; to reduce redundant changes since branch 19 was based off 17

--- server/routes/invitation.js
+++ server/routes/invitation.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const Invitation = require("../database/models/invitation");
+const Invitation = require("../models/invitation");
 const router = express.Router();
 
+router.post("/user/:id/create",
+    passport.authenticate('jwt', { session: false }),
+    function(req, res, next) {
+        const {id} = req.params;
+        const {to_user_id} = req.body;
+        let to_user_email = req.body.to_user_email ? req.body.to_user_email : null;
+        var objId = mongoose.Types.ObjectId;
+
+        const invite = new Invitation({
+            "from_user": new objId(id),
+            "to_user": new objId(to_user_id),
+            to_user_email
+        });
+        invite.save(function(err) {
+            if (err) return handleError(err);
+            res.json({ type: "success", message: "The invitation was saved."});
+        });
+    }
+);
+
+// Returns pending invitations that were sent to the given user
 router.get("/user/:id",
     passport.authenticate('jwt', { session: false }),
     function(req, res, next) {
@@ -23,6 +44,34 @@ router.get("/user/:id",
                 }
             }
         )
+    }
+);
+
+// Returns contacts that the current user accepted invitations from
+router.get("/user/:id/contacts",
+    passport.authenticate('jwt', { session: false }),
+    function(req, res, next) {
+        const {id} = req.params;
+        var objId = mongoose.Types.ObjectId;
+
+        Invitation.aggregate([
+            { $match: {"to_user": new objId(id), "approved": true } },
+            { $lookup: {
+                from: "users",
+                localField: "from_user",
+                foreignField: "_id",
+                as: "user_data"
+                } 
+        }])
+            .exec(function (err, invitations) {
+                if (err) {
+                    return handleError(err);
+                } 
+                if (invitations && invitations.length) {
+                    let contacts = invitations.map(invite => { return invite.user_data[0]})
+                    res.json({ type: "success", contacts})
+                }
+            })
     }
 );
 
