662fd82cf4b4ff05ac980eb94447ad54d74cfc98
Carol Chung <cch5ng@gmail.com>
2020-05-22 15:18:28 -0700

    restructure top level containers to reflect authentication state

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";import Home from "./components/Home/Home";
 import {Theme} from "./themes/Theme";
-import LandingPage from "./pages/Landing";
+import AuthenticatedApp from "./components/AuthenticatedApp/AuthenticatedApp";
+import UnauthenticatedApp from "./components/UnauthenticatedApp/UnauthenticatedApp";
 
 
 function App() {
   return (
     <div>
     <MuiThemeProvider Theme={Theme}>
-      <BrowserRouter>
-        <Route path="/Home" component={Home} />
-        <Route path="/" component={LandingPage} />
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={UnauthenticatedApp} />
+          <Route path="/authenticated" component={AuthenticatedApp} />
+        </Switch>
+      </Router>
     </MuiThemeProvider>
     </div>
   );
