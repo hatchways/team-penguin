65fc7012401957f072701f7399d41170463349dc
Carol Chung <cch5ng@gmail.com>
2020-05-23 15:46:53 -0700

    cleanup

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,35 +9,3 @@ function Home() {
 }
 
 export default Home;
-
-// import React, { Component } from 'react'
-// import Grid from '@material-ui/core/Grid';
-// import Sidebar from '../Sidebar/Sidebar';
-// import Chat from '../Chat/Chat';
-
-// class Home extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Grid container spacing={3}>
-//                 <Grid
-//                     item xs={4}
-//                     //sm={3}
-//                     direction='row'
-//                     //className={props.classes.root}
-//                 >
-//                     <Sidebar/>
-//                 </Grid>
-
-//                 <Grid
-//                     item xs={8}
-//                     direction='row' >
-//                     <Chat/>
-//                 </Grid>
-
-//                 </Grid>
-//             </div>
-//         )
-//     }
-// }
-// export default Home
