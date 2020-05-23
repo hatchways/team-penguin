b96204b451686d8d3b0c1482bbfe53b6a7388243
Carol Chung <cch5ng@gmail.com>
2020-05-23 15:42:31 -0700

    use auth state from context to show conditionally authenticated app vs unauthenticated app

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,31 +1,43 @@
-import React, { Component } from 'react'
-import Grid from '@material-ui/core/Grid';
-import Sidebar from '../Sidebar/Sidebar';
-import Chat from '../Chat/Chat';
+import React from 'react';
+import {useAuthState} from '../../context/auth-context';
+import AuthenticatedApp from '../AuthenticatedApp/AuthenticatedApp';
+import UnauthenticatedApp from '../UnauthenticatedApp/UnauthenticatedApp';
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Grid container spacing={3}>
-                <Grid
-                    item xs={4}
-                    //sm={3}
-                    direction='row'
-                    //className={props.classes.root}
-                >
-                    <Sidebar/>
-                </Grid>
-
-                <Grid
-                    item xs={8}
-                    direction='row' >
-                    <Chat/>
-                </Grid>
-
-                </Grid>
-            </div>
-        )
-    }
+function Home() {
+  const {user} = useAuthState()
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
 }
-export default Home
+
+export default Home;
+
+// import React, { Component } from 'react'
+// import Grid from '@material-ui/core/Grid';
+// import Sidebar from '../Sidebar/Sidebar';
+// import Chat from '../Chat/Chat';
+
+// class Home extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Grid container spacing={3}>
+//                 <Grid
+//                     item xs={4}
+//                     //sm={3}
+//                     direction='row'
+//                     //className={props.classes.root}
+//                 >
+//                     <Sidebar/>
+//                 </Grid>
+
+//                 <Grid
+//                     item xs={8}
+//                     direction='row' >
+//                     <Chat/>
+//                 </Grid>
+
+//                 </Grid>
+//             </div>
+//         )
+//     }
+// }
+// export default Home
