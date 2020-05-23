991d091b9779f67e7b972b1965ebcb58ab46acdc
Carol Chung <cch5ng@gmail.com>
2020-05-23 16:30:20 -0700

    test getting data from context

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
 import Sidebar from '../Sidebar/Sidebar';
 import Chat from '../Chat/Chat.js';
+import { useAuthState} from '../../context/auth-context';
 
 const AuthenticatedApp = () => {
+    let authState = useAuthState()
+    console.log('authState', authState)
+
     return (
         <div>
-            <Grid container spacing={3}>
-                <Grid
-                    item xs={4}
-                    direction='row'
-                >
-                    <Sidebar/>
-                </Grid>
+                    <Grid container spacing={3}>
+                        <Grid
+                            item xs={4}
+                            direction='row'
+                        >
+                            <Sidebar/>
+                        </Grid>
 
-                <Grid
-                    item xs={8}
-                    direction='row' >
-                    <Chat />
-                </Grid>
-            </Grid>
+                        <Grid
+                            item xs={8}
+                            direction='row' >
+                            <Chat />
+                            <h1>Username: {authState.user.username}</h1>
+
+                        </Grid>
+                    </Grid>
         </div>
     )
 }
