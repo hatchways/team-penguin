af3cf0567b2595666b4e522408ea9fb38a73325a
Carol Chung <cch5ng@gmail.com>
2020-05-23 16:30:53 -0700

    cleanup

--- client/src/components/UnauthenticatedApp/UnauthenticatedApp.js
+++ client/src/components/UnauthenticatedApp/UnauthenticatedApp.js
@@ -9,8 +9,6 @@ import {
 import {theme} from "../../themes/theme";
 import Login from "../Login/Login";
 import Signup from "../Signup/Signup";
-//import UnauthenticatedSidebar from '../UnauthenticatedSidebar/UnauthenticatedSidebar';
-// import UnauthenticatedMain from '../UnauthenticatedMain/UnauthenticatedMain.js';
 
 const UnauthenticatedApp = () => {
   return (
@@ -29,24 +27,3 @@ const UnauthenticatedApp = () => {
 }
 
 export default UnauthenticatedApp
-
-/*
-        <div>
-            <Grid container spacing={3}>
-                <Grid
-                    item xs={4}
-                    direction='row'
-                >
-                    <UnauthenticatedSidebar/>
-                </Grid>
-
-                <Grid
-                    item xs={8}
-                    direction='row' >
-                    <UnauthenticatedMain/>
-                </Grid>
-            </Grid>
-        </div>
-    )
-
-*/
