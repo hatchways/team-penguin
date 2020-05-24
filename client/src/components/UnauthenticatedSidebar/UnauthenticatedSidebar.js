50cc0bcfa72244d7d7fe1f03e38bd34fd286b6a5
Rajat Bansal <rjtbansal@gmail.com>
2020-05-24 12:14:49 -0400

    displaying image on the left of sign up page

--- client/src/components/UnauthenticatedSidebar/UnauthenticatedSidebar.js
+++ client/src/components/UnauthenticatedSidebar/UnauthenticatedSidebar.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import messengerImg from '../../assets/messenger-img.png';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  image: {
+    backgroundImage: `url(${messengerImg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh'
+  }
+}));
 
 const UnauthenticatedSidebar = props => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Unauthenticated image</h1>
+    <div className={ classes.image }>
+      {/* <h1>Unauthenticated image</h1> */}
     </div>
   );
 }
