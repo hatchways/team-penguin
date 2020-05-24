48c62b851ddf3bb707ec3bbac2b7f0689b638433
Carol Chung <cch5ng@gmail.com>
2020-05-24 07:08:11 -0700

    Fe skeleton context 36 (#37)
    * add auth context per article by kent dodds
    
    * refactor to use auth provider from react context
    
    * use auth state from context to show conditionally authenticated app vs unauthenticated app
    
    * modify layout so that there is a col on left for static image
    
    * move routes to this file for the unauthenticated app
    
    * cleanup
    
    * test getting data from context
    
    * cleanup
    
    * toggle test condition
    
    * success testing passing function from context to provider
    
    * cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,16 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Home from "./components/Home/Home";
-import {theme} from "./themes/theme";
-import AuthenticatedApp from "./components/AuthenticatedApp/AuthenticatedApp";
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
+import React from 'react';
+import Home from './components/Home/Home';
+import './App.css';
+import {AuthProvider} from './context/auth-context';
 
 function App() {
   return (
-    <div>
-    <MuiThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Signup} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route path="/authenticated" component={AuthenticatedApp} />
-        </Switch>
-      </Router>
-    </MuiThemeProvider>
-    </div>
-  );
+    <AuthProvider>
+      <div>
+        <Home />
+      </div>
+    </AuthProvider>
+  )
 }
 
 export default App;
--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
 import Sidebar from '../Sidebar/Sidebar';
 import Chat from '../Chat/Chat.js';
+import { useAuth } from '../../context/auth-context';
 
 const AuthenticatedApp = () => {
+    let {logout, user} = useAuth();
+
     return (
         <div>
             <Grid container spacing={3}>
@@ -18,8 +21,13 @@ const AuthenticatedApp = () => {
                     item xs={8}
                     direction='row' >
                     <Chat />
+                    <h1>Username: {user.username}</h1>
+                    <button onClick={logout}>test logout
+                    </button> 
+
                 </Grid>
             </Grid>
+
         </div>
     )
 }
