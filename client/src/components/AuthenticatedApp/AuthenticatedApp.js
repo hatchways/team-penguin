080a5fa355fee617eb3f167afa5815f7a497ed75
Carol Chung <cch5ng@gmail.com>
2020-05-23 18:30:15 -0700

    cleanup

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -2,30 +2,32 @@ import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
 import Sidebar from '../Sidebar/Sidebar';
 import Chat from '../Chat/Chat.js';
-import { useAuthState} from '../../context/auth-context';
+import { useAuth } from '../../context/auth-context';
 
 const AuthenticatedApp = () => {
-    let authState = useAuthState()
-    console.log('authState', authState)
+    let {logout, user} = useAuth();
 
     return (
         <div>
-                    <Grid container spacing={3}>
-                        <Grid
-                            item xs={4}
-                            direction='row'
-                        >
-                            <Sidebar/>
-                        </Grid>
+            <Grid container spacing={3}>
+                <Grid
+                    item xs={4}
+                    direction='row'
+                >
+                    <Sidebar/>
+                </Grid>
 
-                        <Grid
-                            item xs={8}
-                            direction='row' >
-                            <Chat />
-                            <h1>Username: {authState.user.username}</h1>
+                <Grid
+                    item xs={8}
+                    direction='row' >
+                    <Chat />
+                    <h1>Username: {user.username}</h1>
+                    <button onClick={logout}>test logout
+                    </button> 
 
-                        </Grid>
-                    </Grid>
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
