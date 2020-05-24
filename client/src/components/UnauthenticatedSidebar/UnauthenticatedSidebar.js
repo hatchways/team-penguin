232c7f914f6249c0e529b83c1820254a253ff8d3
Rajat Bansal <rjtbansal@gmail.com>
2020-05-24 19:29:18 -0400

    using the proper font for image heading in unauthenticated sidebar

--- client/src/components/UnauthenticatedSidebar/UnauthenticatedSidebar.js
+++ client/src/components/UnauthenticatedSidebar/UnauthenticatedSidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import messengerImg from '../../assets/messenger-img.png';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   image: {
     backgroundImage: `linear-gradient(rgba(58, 141, 255,0.85),rgba(134, 185, 255,1)),url(${messengerImg})`,
     backgroundRepeat: 'no-repeat',
@@ -14,8 +14,9 @@ const useStyles = makeStyles(() => ({
   },
   imageHeading: {
     color: 'white',
-    padding: '0 2rem',
-    textAlign: 'center'
+    padding: '0 6rem',
+    textAlign: 'center',
+    fontWeight: theme.typography.fontWeightMedium
   }
 }));
 
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -3,14 +3,7 @@ import { createMuiTheme } from "@material-ui/core";
 export const theme = createMuiTheme({
   typography: {
     fontFamily: '"Open Sans"',
-    fontSize: 12,
-    h1: {
-      fontWeight: 600
-      // could customize the h1 variant as well
-    },
-    // btn: {
-    //   fontWeight: 600
-    // }
+    fontSize: 12
   },
   palette: {
     primary: { main: "#3A8DFF" }
