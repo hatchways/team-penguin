fe850d7a72f6f4476cb2d0f6b610c8505d47b643
Carol Chung <cch5ng@gmail.com>
2020-05-24 17:48:22 -0700

    comment out contacts again because of mult errors

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -3,12 +3,14 @@ import Grid from '@material-ui/core/Grid';
 
 import Contacts from './Contacts/Contacts';
 
+const contacts = [];
+const selectedContact;
 
 const Sidebar = props => {
   return (
     <div>
       <h1>left pane here</h1>
-    
+
       {/* <Contacts 
         contacts={props.contacts}
         selected={props.selected}
