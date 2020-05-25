78eec410e20c1bdca09538e4caa4190684a4eb62
Carol Chung <cch5ng@gmail.com>
2020-05-24 17:36:34 -0700

    comment out routes until figure out what route driven data would be

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid';
+import { MuiThemeProvider } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import {theme} from "../../themes/theme";
 import Sidebar from '../Sidebar/Sidebar';
 import Chat from '../Chat/Chat.js';
 import { useAuth } from '../../context/auth-context';
+
+const messages = [
+    {id: '0', body: 'test msg 1'},
+    {id: '1', body: 'test msg 2'},
+    {id: '2', body: 'test msg 3'}
+];
+//selectedContact should probably be an array to handle group convos too
+const selectedContact = {username: 'test selected contact'};
 
 const AuthenticatedApp = () => {
     let {logout, user} = useAuth();
@@ -20,10 +35,7 @@ const AuthenticatedApp = () => {
                 <Grid
                     item xs={8}
                     direction='row' >
-                    <Chat />
-                    <h1>Username: {user.username}</h1>
-                    <button onClick={logout}>test logout
-                    </button> 
+                    <Chat messages={messages} user={user} selectedContact={selectedContact} />
 
                 </Grid>
             </Grid>
@@ -31,4 +43,23 @@ const AuthenticatedApp = () => {
         </div>
     )
 }
+
 export default AuthenticatedApp;
+
+/*
+                    <h1>Username: {user.username}</h1>
+                    <button onClick={logout}>test logout
+                    </button> 
+*/
+
+/*
+shared state, user and/or contacts
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Chat} />
+          <Route exact path="/conversations" component={Chat} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
+*/
