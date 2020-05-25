7e8b36a034a1d992570030aa0f4f29c315b61335
Carol Chung <cch5ng@gmail.com>
2020-05-25 12:50:37 -0700

    style updates for title, close icon, copy btn

--- client/src/components/Invitations/InvitationDialog.js
+++ client/src/components/Invitations/InvitationDialog.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import {theme} from "../../themes/theme";
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Fab from '@material-ui/core/Fab';
+//import DialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import NavigationIcon from '@material-ui/icons/Navigation';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 //REMOVE
 const testToUid = '5ec815fdfd43011d98648662';
@@ -16,23 +21,65 @@ const testFromUid = '5ec816abfd43011d98648663';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        '& > *': {
-          margin: theme.spacing(1),
-        }
+      '& > *': {
+        margin: theme.spacing(1),
+      },
     },
-    margin: {
-      margin: theme.spacing(1),
+    labelRoot: {
+      fontSize: 30,
+      color: "red",
+      "&$labelFocused": {
+        color: "purple"
+      }
     },
-    extendedIcon: {
-      marginRight: theme.spacing(1),
-    },
-    dialogContent: {
-      width: '80%'
+    btn: {
+      fontWeight: '600'
     },
     btnMixedCase: {
-      textTransform: 'unset !important'
+      textTransform: 'unset !important',
+      fontWeight: '600',
+      fontSize: theme.spacing(2),
     },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.action.disabled, //theme.palette.grey[500],
+    },
+    btnOverlay: {
+      zIndex: '2000',
+      position: 'absolute',
+      right: theme.spacing(4),
+      top: theme.spacing(35.5),
+    }
   }));
+
+  const dialogTitleStyles = (theme) => ({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    title: {
+      color: theme.palette.text.primary,
+      align: 'center',
+      margin: theme.spacing(3),
+      fontWeight: 'bold'
+    }
+  });
+
+  const DialogTitle = withStyles(dialogTitleStyles)((props) => {
+    const { children, classes, ...other } = props;
+    return (
+      <MuiDialogTitle disableTypography className={classes.root} {...other}>
+        <div>
+          <Typography variant="h5" className={classes.title}
+            align="center">
+              {children}
+          </Typography>
+        </div>
+      </MuiDialogTitle>
+    );
+  });
 
 export default function InvitationDialog() {
   const [open, setOpen] = React.useState(false);
@@ -48,8 +95,12 @@ export default function InvitationDialog() {
     setOpen(true);
   };
 
-  //handle submit form
   const handleClose = (ev) => {
+    setOpen(false);
+  }
+
+  //handle submit form
+  const handleSave = (ev) => {
     ev.preventDefault();
     //TODO
     let jwtToken = '';
@@ -74,7 +125,13 @@ export default function InvitationDialog() {
       },
       body: JSON.stringify(body)
     })
-    setOpen(false);
+      .then(resp => {
+        setEmail('');
+        setOpen(false);
+      })
+      .catch(err => {
+        console.error(err)
+      })
   };
 
   const getEmailAr = (emailStr) => {
@@ -97,19 +154,24 @@ export default function InvitationDialog() {
         }
       }
     });
-
   }
 
-  //        contentstyle={{width: "100%", maxWidth: "none"}}>
-  //maxWidth="large"
   return (
     <div>
       <Button color="primary" onClick={handleClickOpen} className={classes.btnMixedCase}>
         + Invite Friends
       </Button>
       <Dialog open={open} onClose={handleClose} 
-        aria-labelledby="form-dialog-title" >
-        <DialogTitle id="form-dialog-title">Invite Friends to Messenger</DialogTitle>
+        aria-labelledby="form-dialog-title"
+        fullWidth
+        maxWidth='sm'>
+
+        <IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+        <DialogTitle id="form-dialog-title" onClose={handleClose}>
+          Invite Friends to Messenger
+        </DialogTitle>
         <DialogContent className={classes.dialogContent}>
           <DialogContentText>
             Send your friends an invite email.
@@ -123,6 +185,7 @@ export default function InvitationDialog() {
             value={email}
             onChange={handleChange}
             helperText="Separate multiple emails with a comma."
+            InputLabelProps={{style: {fontSize: 18, color: '#000'}}}
             fullWidth
           />
         </DialogContent>
@@ -140,23 +203,29 @@ export default function InvitationDialog() {
             }}
             variant="filled"
             fullWidth
+            InputLabelProps={{
+              classes: {
+                root: classes.labelRoot,
+                focused: classes.labelRoot
+              }
+            }}
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} 
+          <Button
             size="small"
             color="primary" 
             variant="contained"
             id="referral-url-btn"
             value={referralUrl}
             onClick={handleClickCopyBtn}
-            >
+            className={classes.btnOverlay}>
             Copy Link
           </Button>
         </DialogActions>
 
         <DialogActions>
-          <Button onClick={handleClose} color="primary" variant="contained"
+          <Button onClick={handleSave} color="primary" variant="contained"
             className={classes.btnMixedCase} size="lg">
             Send Invite
           </Button>
