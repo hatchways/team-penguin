64d15b79fea672e383e8442fa3a2e444d13a52ac
Carol Chung <cch5ng@gmail.com>
2020-05-24 18:17:16 -0700

    add dummy friends data to render content

--- client/src/components/Sidebar/Contacts/Contacts.js
+++ client/src/components/Sidebar/Contacts/Contacts.js
@@ -7,17 +7,20 @@ import Friends from './Friends';
 import Requests from './Requests';
 import Pending from './Pending';
 
-
-
 const Contacts = props => {
   const [display, setDisplay] = React.useState('friends');
   const toggleDisplay = (event, setting) => {
     setDisplay(setting);
   }
 
-  //const friends; //= props.contacts.filter(curr => curr.status === 3);
-  //const requests; //= props.contacts.filter(curr => curr.status === 2);
-  //const pending; //= props.contacts.filter(curr => curr.status === 1);
+  const friends = [
+    {username: 'test friend1'},
+    {username: 'test friend2'},
+    {username: 'test friend3'}
+  ]
+  //const friends = props.contacts.filter(curr => curr.status === 3);
+  //const requests = props.contacts.filter(curr => curr.status === 2);
+  //const pending = props.contacts.filter(curr => curr.status === 1);
 
   return (
     <Grid
@@ -67,7 +70,7 @@ const Contacts = props => {
 
       {display === 'friends' && 
         <Friends 
-          //friends={friends}
+          friends={friends}
           selected={props.selected} 
           requestContact={props.requestContact}
           selectContact={props.selectContact}
