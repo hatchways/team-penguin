ac9ee8f3573cb33618fd631f815c52a38e318f2d
Carol Chung <cch5ng@gmail.com>
2020-05-24 18:18:23 -0700

    enable content display except for avatar

--- client/src/components/Sidebar/Contacts/Friends.js
+++ client/src/components/Sidebar/Contacts/Friends.js
@@ -8,20 +8,20 @@ import Modal from '@material-ui/core/Modal';
 
 
 const Friends = props => {
-  // const entries = (selected, select) => props.friends.map(curr => (
-  //   <Grid
-  //     item
-  //     container
-  //     spacing={3}
-  //     alignItems='center'
+  const entries = (selected, select) => props.friends.map(curr => (
+    <Grid
+      container
+      spacing={3}
+      alignItems='center'
   //     // className={
   //     //   selected === curr.username 
   //     //   ? props.classes.friend + ' ' + props.classes.selected 
   //     //   : props.classes.friend
   //     // }
-  //     key={curr.username}
-  //     onClick={() => {select(curr.index)}}
-  //   >
+      key={curr.username}
+      onClick={() => {select(curr.index)}}
+    >
+  {/* 
   //     <Grid item>
   //       {curr.image ? (
   //         <Avatar 
@@ -37,48 +37,47 @@ const Friends = props => {
             
   //         </Avatar>
   //       )}
-  //     </Grid>
-  //     <Grid item>
-  //       <Typography variant='h5'>{curr.username}</Typography>
-  //     </Grid>
-  //   </Grid>
-  // ));
+  //     </Grid>*/}
+      <Grid item>
+        <Typography variant='h5'>{curr.username}</Typography>
+      </Grid>
+    </Grid>
+  ));
 
-  // const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-  // const openModal = () => {
-  //   setOpen(true);
-  // }
+  const openModal = () => {
+    setOpen(true);
+  }
 
-  // const closeModal = () => {
-  //   setOpen(false);
-  // }
+  const closeModal = () => {
+    setOpen(false);
+  }
 
-  // return (
-  //   <Grid 
-  //     item 
-  //     container
-  //     direction='column'
-  //   >
-  //     <Modal 
-  //       open={open} 
-  //       onClose={closeModal} 
-  //      // className={props.classes.friendModal}
-  //     >
+  return (
+    <Grid 
+      item 
+      container
+      direction='column'
+    >
+      <Modal 
+        open={open} 
+        onClose={closeModal} 
+       // className={props.classes.friendModal}
+      >
         
-  //     </Modal>
-  //     {/* className={props.classes.addFriend} */}
-  //     <Grid item >
-  //       <Button color='primary' onClick={openModal} disableRipple>
-  //       {/* classes={{root: props.classes.addFriendIcon}} */}
-  //         <Icon className='fas fa-plus' />
-  //         <Typography variant='body1'>Add friend</Typography>
-  //       </Button>
-  //     </Grid>
-  //     {entries(props.selected, props.selectContact)}
-  //   </Grid>
-  // );
-  return (null);
+      </Modal>
+      {/* className={props.classes.addFriend} */}
+      <Grid item >
+        <Button color='primary' onClick={openModal} disableRipple>
+        {/* classes={{root: props.classes.addFriendIcon}} */}
+          <Icon className='fas fa-plus' />
+          <Typography variant='body1'>Add friend</Typography>
+        </Button>
+      </Grid>
+      {entries(props.selected, props.selectContact)}
+    </Grid>
+  );
 }
 
 export default Friends;
