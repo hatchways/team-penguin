15322da743e40dce444375cb11628d04b221cae7
Carol Chung <cch5ng@gmail.com>
2020-05-26 09:27:11 -0700

    stub out logic for invitation creation; email validation; initial creation of invitations for existing users (in progress)

--- server/routes/invitation.js
+++ server/routes/invitation.js
@@ -1,15 +1,73 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const validator = require("validator");
 const Invitation = require("../models/invitation");
 const router = express.Router();
 
-router.post("/user/:email/create",
+router.post("/user/:email",
     passport.authenticate('jwt', { session: false }),
     function(req, res, next) {
         const {email} = req.params;
-        const {to_user_id} = req.body;
-        let to_user_email = req.body.to_user_email ? req.body.to_user_email : null;
+        const {emailAr, fromEmail} = req.body;
+        let invalidEmails = [];
+        let validEmails = [];
+        let curUserEmails = [];
+        let nonCurUserEmails = [];
+
+//test with one recipient
+//test with multiple recipients (mix of existing users, nonexisting users)
+
+/*
+email validation
+    no empty arr
+    valid email values
+list of valid emails
+list of invalid emails
+*/
+        if (emailAr.length === 0) {
+            return res.status(400).json({error: 'No email addresses for invitation recipients were provided.'})
+        }
+        emailAr.forEach(email => {
+            if (validator.isEmail(email)) {
+                validEmails.push(email);
+            } else {
+                invalidEmails.push(email);
+            }
+        })
+
+/*
+get list of recipients who are already users
+make list of recipients who are not users
+*/
+        if (!validEmails.length) {
+            return res.status(400).json({error: 'Email addresses for invitation recipients were invalid. Please check the spelling.'})
+        } else {
+
+//query for existing users (TODO)
+    //sendgrid for non existing users
+
+    //create invites for existing users
+            let newInvites = [];
+            validEmails.forEach(to_user_email => {
+                let newInvite = {to_user_email, from_user_email: fromEmail};
+                newInvites.push(newInvite);
+            });
+            Invitation.insertMany(newInvites, function(err, invitations) {
+                if (err) console.error(err);
+
+            })
+
+        }
+
+/*
+create invitations for recipients who are users
+    then trigger sendgrid requests for recipients who are not users
+*/
+
+
+
+
 
         const invite = new Invitation({
             "from_user_email": email,
