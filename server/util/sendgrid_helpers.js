0dc2fb0167747a41d2c1f93b314383c5986758e9
Carol Chung <cch5ng@gmail.com>
2020-05-26 10:26:47 -0700

    init vers of sendEmailMultiple func

--- server/util/sendgrid_helpers.js
+++ server/util/sendgrid_helpers.js
@@ -13,8 +13,21 @@ const sendEmail = ({from_email, to_email, referral_id}) => {
       .catch(err => console.log(err));
 }
 
-const sendEmailMultiple = () => {
-
+const sendEmailMultiple = ({from_email, to_email_ar, referral_id}) => {
+  let messages = to_email_ar.map(to_email => {
+    let msg = {
+      to: `${to_email}`,
+      from: 'hatchways.tester99@gmail.com',
+      subject: 'World messenger invitation',
+      text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! localhost:3000/join/${referral_id}`,
+    };
+  
+    return msg;
+  })
+  Promises.all(messages.map(msg => sgMail.send(msg)))
+    .then(resp => resp.json())
+    .then(json => console.log('sendgrid group results', json))
+    .catch(err => console.error('sendgrid errors', err))
 }
 
-module.exports = {sendEmail};
+module.exports = {sendEmail, sendEmailMultiple};
