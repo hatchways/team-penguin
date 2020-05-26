7b63035104fc80b0cf24ffb3459507f3cb70158f
Carol Chung <cch5ng@gmail.com>
2020-05-26 09:49:04 -0700

    stub out init changes to sendgrid request (for one email)

--- server/util/test_sendgrid.js
+++ server/util/test_sendgrid.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
-
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'example@email.com',
-  from: 'hatchways.tester99@gmail.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg)
-    .then(resp => console.log(resp))
-    .catch(err => console.log(err));
+
+export const sendEmail = (from_email, to_email, referral_id) => {
+  let msg = {
+    to: `${to_email}`,
+    from: 'hatchways.tester99@gmail.com',
+    subject: 'World messenger invitation',
+    text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! localhost:3000/join/${referral_id}`,
+  };
+  sgMail.send(msg)
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err));
+}
