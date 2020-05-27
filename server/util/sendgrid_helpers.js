8f076438d96645bb65bab1c9af0796ff5d310b66
Carol Chung <cch5ng@gmail.com>
2020-05-27 15:13:01 -0700

    refactor sendgrid helpers, sendEmail() to handle multiple messages

--- server/util/sendgrid_helpers.js
+++ server/util/sendgrid_helpers.js
@@ -1,28 +1,22 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = ({from_email, to_email, referral_id}) => {
-  let msg = {
-    to: `${to_email}`,
+const sendEmail = ({from_email, to_email_ar, referral_id}) => {
+  // let msg = {
+  //   to: `${to_email}`,
+  //   from: 'hatchways.tester99@gmail.com',
+  //   subject: 'World messenger invitation',
+  //   text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! http://localhost:3000/join/${referral_id}`,
+  // };
+  let messages = to_email_ar.map(to_email => (
+    {to: `${to_email}`,
     from: 'hatchways.tester99@gmail.com',
     subject: 'World messenger invitation',
-    text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! http://localhost:3000/join/${referral_id}`,
-  };
-  return sgMail.send(msg);
+    text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! http://localhost:3000/join/${referral_id}`,}
+  ))
+
+
+  return sgMail.send(messages);
 }
 
-const sendEmailMultiple = ({from_email, to_email_ar, referral_id}) => {
-  let messages = to_email_ar.map(to_email => {
-    let msg = {
-      to: `${to_email}`,
-      from: 'hatchways.tester99@gmail.com',
-      subject: 'World messenger invitation',
-      text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! http://localhost:3000/join/${referral_id}`,
-    };
-  
-    return msg;
-  })
-  return Promise.all(messages.map(msg => sgMail.send(msg)));
-}
-
-module.exports = {sendEmail, sendEmailMultiple};
+module.exports = {sendEmail};
