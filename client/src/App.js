5241031efeed81de1d7217e39e3f936de6ec83e3
Carol Chung <cch5ng@gmail.com>
2020-05-27 13:16:07 -0700

    stub init attempt to connect client to server on socket; in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import socketIOClient from "socket.io-client";
 import Home from './components/Home/Home';
 import './App.css';
 import {AuthProvider} from './context/auth-context';
 
 function App() {
+
+  useEffect(() => {
+    const socket = socketIOClient();
+    
+  }, [])
   return (
     <AuthProvider>
       <div>
