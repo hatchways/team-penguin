d66180dfd063d0ab240edd891a81b16b38791443
Carol Chung <cch5ng@gmail.com>
2020-05-27 18:36:07 -0700

    debug logic in post route to handle referral registration

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const User = require('../models/user');
+const Invitation = require('../models/invitation');
+
 const registrationValidator = require('../controllers/userRegistrationValidator');
 const loginValidator = require('../controllers/userLoginValidator');
 const secretKey = require('../db-credentials.json');
@@ -91,46 +94,49 @@ router.get("/:fromEmail/referralId",
   }
 );
 
-router.post('/register/referral', (req, res) => {
+router.post('/register/referral',
+  //passport.authenticate('jwt', { session: false }),
+  (req, res) => {
   const {validationErrors, isRegistrationValid} = registrationValidator(req.body);
-  const {referralId} = req.body;
+  const {referralId, email, password, language} = req.body;
 
   if(!isRegistrationValid) {
     return res.status(400).json(validationErrors);
   }
 
-  User.findOne({ email: req.body.email })
+  User.findOne({ email })
       .then(user => {
         if(user) {
-          return res.status(400).json({email: "Email you entered already exists"});
+          return res.status(400).json({email: "The email you entered already exists"});
         }
         else {
           const newUser = new User({
-            email: req.body.email,
-            password: req.body.password,
-            language: req.body.language
+            email,
+            password,
+            language
           });
           bcrypt.genSalt(12, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
               if(err) throw err;
               newUser.password = hash;
-              newUser.save()
-                .then(user => {
-                  User.findOne({referralId})
-                    .then(user => {
+              newUser.save(function(err) {
+                if (err) return console.error('Invitation recipient could not be registered', err);
+                User.findOne({referral_id: mongoose.Types.ObjectId(referralId)},
+                    if (err) return console.error('Invitation sender could not be found', err)
+                    if (user._id) {
                       let from_user_email = user.email;
-                      let invite = {from_user_email, to_user_email: email};
-                      Invitation.save(invite)
+                      let invite = new Invitation({from_user_email, to_user_email: email});
+                      invite.save()
                         .then(invitation => {
-                          res.status(201).json({type: 'success', message: `${user.email} was  registered. An invitation from ${from_user_email} was created.`})
+                          res.status(201).json({type: 'success', message: `${email} was  registered. An invitation from ${from_user_email} was created.`})
                         })
-                        .catch(err => console.error('New invitation could not be created.', err));
-                    })
-                    .catch(err => console.error('Invitation sender could not be found', err))
-                })
-                .catch(err => console.error(err));
+                        .catch(err => console.error(`A new invitation could not be created for ${email}.`, err));  
+                    }
+                  }
+                )
+              });
             });
-          });
+          })
         }
       });
 });
