49c6aa8c945840f8a90f59fbb920755588898264
Carol Chung <cch5ng@gmail.com>
2020-05-28 16:32:38 -0700

    set up initial test connection between client and server; verified messages sent 2 ways

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
+  socket.on('news', (data) => {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
