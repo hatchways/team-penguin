4c806b5e5addba4e43387b993db43fe680f69f00
Carol Chung <cch5ng@gmail.com>
2020-05-27 17:34:59 -0700

    cleanup

--- server/util/sendgrid_helpers.js
+++ server/util/sendgrid_helpers.js
@@ -2,20 +2,12 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = ({from_email, to_email_ar, referral_id}) => {
-  // let msg = {
-  //   to: `${to_email}`,
-  //   from: 'hatchways.tester99@gmail.com',
-  //   subject: 'World messenger invitation',
-  //   text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! http://localhost:3000/join/${referral_id}`,
-  // };
   let messages = to_email_ar.map(to_email => (
     {to: `${to_email}`,
     from: 'hatchways.tester99@gmail.com',
     subject: 'World messenger invitation',
     text: `${from_email} has invited you to connect on World messenger! Here’s the link to it! http://localhost:3000/join/${referral_id}`,}
   ))
-
-
   return sgMail.send(messages);
 }
 
