a1d09a28b6f8da333f73875d64be64dfaff3a081
Carol Chung <cch5ng@gmail.com>
2020-05-29 16:41:20 -0700

    add language toggle

--- client/src/components/Chat/ChatHeader.js
+++ client/src/components/Chat/ChatHeader.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, {useState} from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
 import { deepOrange, deepPurple } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,29 +27,87 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
+
 const ChatHeader = props => {
   const {selectedContacts} = props;
-  let initial = selectedContacts.length === 1 ? selectedContacts[0].email[0].toUpperCase(): null;
   const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedC: true,
+  });
+  let initial;
+  let email;
+  if (selectedContacts.length === 1) {
+    initial = selectedContacts[0].email[0].toUpperCase();
+    email = selectedContacts[0].email;
+  };
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return (
     <Grid
       item
       container
       spacing={3}
+      direction="row"
       alignItems='center'
       //className={props.classes.root}
       className='sectionHeader'
     >
       {selectedContacts.length === 1 && (
-        <Grid item>
-          <Avatar className={classes.orange}>{initial}</Avatar>
-        </Grid>
+        <React.Fragment>
+          <Grid item>
+            <Avatar className={classes.orange}>{initial}</Avatar>
+          </Grid>
+          <Grid item>
+            <Typography variant='h5'>{email}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography component="div">
+              <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item>Off</Grid>
+                <Grid item>
+                  <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+                </Grid>
+              </Grid>
+            </Typography>
+          </Grid>
+        </React.Fragment>
       )}
-  
-      <Grid item>
-        <Typography variant='h5'>{selectedContacts.email}</Typography>
-      </Grid>
-  
     </Grid>
   );
 }
