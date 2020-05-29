d2c1797231a1db34e88c34bf45b2e541417307ef
Carol Chung <cch5ng@gmail.com>
2020-05-29 16:05:24 -0700

    removed padding around app

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -7,6 +7,7 @@ import {
   Route
 } from "react-router-dom";
 import {theme} from "../../themes/theme";
+import { makeStyles } from '@material-ui/core/styles';
 import Sidebar from '../Sidebar/Sidebar';
 import Chat from '../Chat/Chat.js';
 import { useAuth } from '../../context/auth-context';
@@ -18,32 +19,41 @@ const messages = [
     {id: '2', body: 'test msg 3'}
 ];
 //selectedContact should probably be an array to handle group convos too
-const selectedContact = {username: 'test selected contact'};
+const selectedContacts = [{email: 'friend'}];
+//[{email: 'friend1'}, {email: 'friend2'}, {email: 'friend2'}]
 
 const divStyle = {
   backgroundColor: 'rgba(0, 0, 0, 0.04)',
   height: '100vh',
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: '0', //theme.spacing(1),
+    },
+  },
+}))
+
 const AuthenticatedApp = () => {
     let {logout, user} = useAuth();
+    const classes = useStyles();
 
     return (
-        <div className="app-authenticated">
-            <MuiThemeProvider theme={theme}>
-                <Grid container 
-                    spacing={3} direction='row' 
-                    >
-                    <Grid item xs={4} style={divStyle}>
-                        <Sidebar/>
-                    </Grid>
+      <MuiThemeProvider theme={theme} >
+          <Grid container 
+              spacing={0} direction='row' 
+              className={classes.root}>
+              <Grid item xs={12} sm={4} style={divStyle}>
+                  <Sidebar/>
+              </Grid>
 
-                    <Grid item xs={8}>
-                        {/* <Chat messages={messages} user={user} selectedContact={selectedContact} /> */}
-                    </Grid>
-                </Grid>
-            </MuiThemeProvider>
-        </div>
+              <Grid item xs={12} sm={8}>
+                  <Chat messages={messages} user={user} selectedContacts={selectedContacts} />
+              </Grid>
+          </Grid>
+      </MuiThemeProvider>
     )
 }
 
