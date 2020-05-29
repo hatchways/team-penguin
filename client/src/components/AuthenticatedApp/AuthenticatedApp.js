498f0985ccc2c41e6d19f05bc7c93e22abe7a422
Carol Chung <cch5ng@gmail.com>
2020-05-29 16:42:00 -0700

    add padding back to grid

--- client/src/components/AuthenticatedApp/AuthenticatedApp.js
+++ client/src/components/AuthenticatedApp/AuthenticatedApp.js
@@ -49,7 +49,7 @@ const AuthenticatedApp = () => {
                   <Sidebar/>
               </Grid>
 
-              <Grid item xs={12} sm={8}>
+              <Grid item xs={12} sm={8} style={{padding: '18px'}}>
                   <Chat messages={messages} user={user} selectedContacts={selectedContacts} />
               </Grid>
           </Grid>
