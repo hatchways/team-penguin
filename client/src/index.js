eb29e65cd377da9fa4905a28ed135ba9f9e4aaef
Carol Chung <cch5ng@gmail.com>
2020-05-28 18:33:26 -0700

    further exploration on custom namespace

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:3001');
-  socket.on('news', (data) => {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
+const socket = io.connect('http://localhost:3001/chat');
+socket.on('chat', (data) => {
+  console.log(data);
+  socket.emit('my other event', { my: 'chat' });
+});
+socket.on('hi', (data) => {
+  console.log(data);
+});
+
+// const socketChat = io.connect('http://localhost:3001/chat');
+// socketChat.connected('news', (data) => {
+//   console.log(data);
+//   socket.emit('only for chat', { my: 'chat' });
+// })
+
+// const socket2 = io('/chat');
+// socket2.emit('my other event', { hi: 'chat' });
+// socket2.on('news', (data) => {
+//   console.log(data);
+// });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
