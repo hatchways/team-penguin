1ddd99908fc23ea9d3accf605486bc75de15f982
Carol Chung <cch5ng@gmail.com>
2020-05-29 17:19:15 -0700

    add style to the text box for broadcasted messages

--- client/src/components/Chat/Message.js
+++ client/src/components/Chat/Message.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 
 const Message = props => (
-  // className={props.classes.root}
-  <Grid item >
-    {/*className={props.classes.message} */}
-    <div >
-      <Typography variant='body1'>{props.message.body}</Typography>
+    <div style={{display: 'flex', flexFlow: 'row nowrap', justifyItems: 'left', alignItems: 'center', margin: '18px 0'}}>
+      <Avatar>S</Avatar>
+      <div style={{display: 'flex', flexFlow: 'column nowrap', marginLeft: '8px'}}>
+        <Typography variant='body1'>12:05</Typography>
+        <div style={{height: '28px', border: '1px solid #fff', backgroundColor: '#00f', borderRadius: '8px', color: '#fff', padding: '8px 16px 0 16px'}}>test message body</div>
+      </div>
     </div>
-  </Grid>
 );
 
 export default Message;
