7b96c7f4ee34da1e217d8b2364fae67b550fca93
Carol Chung <cch5ng@gmail.com>
2020-05-29 17:23:25 -0700

    more styling on text message broadcasted

--- client/src/components/Chat/Message.js
+++ client/src/components/Chat/Message.js
@@ -8,7 +8,7 @@ const Message = props => (
       <Avatar>S</Avatar>
       <div style={{display: 'flex', flexFlow: 'column nowrap', marginLeft: '8px'}}>
         <Typography variant='body1'>12:05</Typography>
-        <div style={{height: '28px', border: '1px solid #fff', backgroundColor: '#00f', borderRadius: '8px', color: '#fff', padding: '8px 16px 0 16px'}}>test message body</div>
+        <div style={{height: '28px', border: '1px solid #fff', backgroundColor: '#00f', borderTopRightRadius: '8px', borderBottomRightRadius: '8px', borderBottomLeftRadius: '8px', color: '#fff', padding: '8px 16px 0 16px'}}>test message body</div>
       </div>
     </div>
 );
