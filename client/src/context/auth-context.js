f38e6a30298e25b2a7fc50771ca2156f1610ddf5
Rajat Bansal <rjtbansal@gmail.com>
2020-06-01 18:27:49 -0400

    updated auth-context with some clalrifying questions

--- client/src/context/auth-context.js
+++ client/src/context/auth-context.js
@@ -2,16 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 
-const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+// const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
-//on session initialization, check for a jwt token,
-  //if exists, assume user status success and BE will validate token on first get request
-  //if not exists, update so status is error and user will be kicked back to login
-const getUser = () => sleep(1000).then(() => ({email: 'elmo'}))
-//comment following line to test authenticated
-.then(() => null)
+// //on session initialization, check for a jwt token,
+//   //if exists, assume user status success and BE will validate token on first get request
+//   //if not exists, update so status is error and user will be kicked back to login
+// const getUser = () => sleep(1000).then(() => ({email: 'elmo'}))
+// //comment following line to test authenticated
+// .then(() => null)
 
-const AuthContext = React.createContext([{}, () => {}])
+const AuthContext = React.createContext([{}, () => {}]);
 function AuthProvider({children}) {
   const [state, setState] = React.useState({
     status: 'pending',
@@ -19,8 +19,18 @@ function AuthProvider({children}) {
     user: null
   });
 
+  //checking for token and email and then accordingly updating the state
+  const getUser = () => {
+    const getToken = localStorage.getItem('authToken');
+    if(getToken) {
+      const userEmail = localStorage.getItem('email');
+      setState({status: 'logged in', error: null, user: userEmail})
+    }
+  }
+
   const logout = () => {
     localStorage.removeItem('authToken');
+    localStorage.removeItem('email');
     setState({status: 'logged out', error: null, user: null})
   }
 
@@ -42,20 +52,18 @@ function AuthProvider({children}) {
     }
   }
 
+  //not sure what this useEffect will be doing here
+  //getUser() logic is now checking for tokens and updating state accordingly
   React.useEffect(() => {
-    //if token exists set user email to '' 
-    
-    // const authToken = localStorage.getItem('authToken');
-    // if(authToken){
-    //   setState({status: 'success', error: null, user: ''})
-    //   //(???) let the first api request from authenticated app handle the jwt validation
-    // }
     getUser().then(
       user => setState({status: 'success', error: null, user}),
       error => setState({status: 'error', error, user: null}),
     )
-  }, [])
+  }, []);
+  
   let authState = {...state, logout, login}
+  //what exactly is the provider doing here?
+  //can we just return above authState to login to identify errors, status and user and act accordingly?
   return (
     <AuthContext.Provider value={authState}>
       {state.status === 'pending' ? (
@@ -85,6 +93,7 @@ function useAuth() {
   return context;
 }
 
+//is this needed since useAuth is using the context??
 function useAuthState() {
   const state = React.useContext(AuthContext)
   const isPending = state.status === 'pending'
