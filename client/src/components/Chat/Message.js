2edc32280d227b5da8d4a0f1414785534d76ae02
Carol Chung <cch5ng@gmail.com>
2020-06-01 18:39:50 -0700

    style message bubble

--- client/src/components/Chat/Message.js
+++ client/src/components/Chat/Message.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+
+const messageBubbleStyle = {
+  height: '28px',
+  border: '1px solid #fff',
+  backgroundColor: '#00f',
+  borderTopRightRadius: '8px',
+  borderBottomRightRadius: '8px',
+  borderBottomLeftRadius: '8px',
+  color: '#fff',
+  padding: '8px 16px 0 16px',
+  background: 'linear-gradient(0.4turn,#2962ff, #82b1ff)'
+}
 
 const Message = props => (
-  // className={props.classes.root}
-  <Grid item >
-    {/*className={props.classes.message} */}
-    <div >
-      <Typography variant='body1'>{props.message.body}</Typography>
+  <div style={{display: 'flex', flexFlow: 'row nowrap', justifyItems: 'left', alignItems: 'flex-start', margin: '18px 0'}}>
+    <Avatar>S</Avatar>
+    <div style={{display: 'flex', flexFlow: 'column nowrap', marginLeft: '8px'}}>
+      <Typography variant='body1'>12:05</Typography>
+      <div style={messageBubbleStyle}>test message body</div>
     </div>
-  </Grid>
+  </div>
 );
 
 export default Message;
