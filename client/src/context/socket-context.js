ae08fd7efc9774ecf12f05be2cec27b26f7229b2
Carol Chung <cch5ng@gmail.com>
2020-06-02 11:15:34 -0700

    moved client socket instance to react context

---
+++ client/src/context/socket-context.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import io from 'socket.io-client';
+
+const SocketContext = React.createContext([{}, () => {}])
+const socket = io.connect('http://localhost:3001/chat');
+
+function SocketProvider({children}) {
+
+  const sendChatMessage = (from_email, message) => {
+    socket.on('chat', (data) => {
+      console.log(data);
+      socket.emit(from_email, { my: message });
+    });
+  }
+
+  const socketShare = {socket, sendChatMessage};
+
+  return (
+    <SocketContext.Provider value={socketShare}>
+      {children}
+    </SocketContext.Provider>
+  )
+}
+
+function useSocket() {
+  const context = React.useContext(SocketContext)
+  if (context === undefined) {
+    throw new Error(`useSocket must be used within a SocketProvider`)
+  }
+  return context;
+}
+
+export {SocketProvider, useSocket};
