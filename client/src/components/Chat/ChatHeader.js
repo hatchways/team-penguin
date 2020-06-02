7e0fdbfea4ea46ca3adc2271d26821f4b845585c
Carol Chung <cch5ng@gmail.com>
2020-06-01 18:36:07 -0700

    update chat header; content and style; still styling

--- client/src/components/Chat/ChatHeader.js
+++ client/src/components/Chat/ChatHeader.js
@@ -1,34 +1,122 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+import Icon from '@material-ui/core/Icon';
+import { deepOrange, deepPurple } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  orange: {
+    color: theme.palette.getContrastText(deepOrange[500]),
+    backgroundColor: deepOrange[500],
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+  sectionHeader: {
+    flexBasis: '70px',
+    flexShrink: '0',
+    backgroundColor: '#fff',
+    marginBottom: '5px',
+    padding: '18px',
+  },
+}));
+
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
 
 const ChatHeader = props => {
-  const {selectedContact} = props;
+  const {selectedContacts} = props;
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedC: true,
+  });
+  let initial;
+  let email;
+  if (selectedContacts.length === 1) {
+    initial = selectedContacts[0].email[0].toUpperCase();
+    email = selectedContacts[0].email;
+  };
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return (
     <Grid
       item
       container
-      spacing={3}
+      spacing={0}
+      direction="row"
       alignItems='center'
-      //className={props.classes.root}
+      className={classes.sectionHeader}
     >
-      {/* <Grid item>
-        {props.selected.image ? (
-          <Avatar
-            alt='avatar'
-            src={(props.selected.image.data.data, props.selected.image.contentType)}
-            className={props.classes.avatar}
-          />
-        ) : (
-          <Avatar alt='avatar' className={props.classes.avatar} />
-        )}
-      </Grid> */}
-  
-      <Grid item>
-        <Typography variant='h4'>{selectedContact.username}</Typography>
-      </Grid>
-  
+      {selectedContacts.length === 1 && (
+        <React.Fragment>
+          <Grid item>
+            <Avatar className={classes.orange}>{initial}</Avatar>
+          </Grid>
+          <Grid item>
+            <Typography variant='h5'>{email}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='h5'>online status todo</Typography>
+          </Grid>
+          <Grid item>
+            <Typography component="div">
+              <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item>Off</Grid>
+                <Grid item>
+                  <AntSwitch checked={state.checkedC} onChange={handleChange} name="checkedC" />
+                </Grid>
+              </Grid>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Icon>more_horiz</Icon>
+          </Grid>
+        </React.Fragment>
+      )}
     </Grid>
   );
 }
