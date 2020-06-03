cb77f392143164154aa9a4cd32634bea09d8778e
Carol Chung <cch5ng@gmail.com>
2020-06-02 18:24:01 -0700

    fix conversation update logic

--- server/util/socket_db.js
+++ server/util/socket_db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 const Conversation = require("../models/conversation");
 
 const saveMessageToConversation = ({message, conversationId}) => {
-  Conversation.update({_id: conversationId}, {
+  const id = mongoose.Types.ObjectId(conversationId);
+  Conversation.update({_id: id}, {
                       $push: {
-                        grades: newGrade
+                        messages: message
                       }
                     }, function(err, chat) {
                       if (err) return console.error('Could not save post', err);
