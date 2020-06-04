0f197eddd7a84a2a751e08080a4603e657c79b82
Rajat Bansal <rjtbansal@gmail.com>
2020-06-03 22:15:31 -0400

    added snackbar when invitation is successful

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Contacts from './Contacts/Contacts';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 import axios from 'axios';
 
 const Sidebar = props => {
-  const contacts = [];
-  const selectedContact = {username: 'test selected contact'};
   const email = localStorage.getItem('email');
-  const [approveInvite, setApproveInvite] = useState(false);
+  const [approveInvite, setApproveInvite] = useState('');
+
+  function closeAlertHandler() {
+    setApproveInvite('');
+  }
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
 
   const updateContact = async(approvedEmail) => {
     const approvedRes = await axios.put(`http://localhost:3001/invitations/user/${email}/approve`, 
                                       {  'from_email': approvedEmail });
     if(approvedRes.data.approved && approvedRes.data.from_user_email === approvedEmail){
       //add snackbar alert
-      console.log(approvedRes.data);
-      setApproveInvite(true);
+      setApproveInvite(`${approvedEmail} is now your friend`);
     }
   }
 
@@ -28,6 +35,11 @@ const Sidebar = props => {
         updateContact={updateContact}
         selectContact={props.selectContact}
       />
+       <Snackbar open = {approveInvite.length !== 0} autoHideDuration={3000} onClose = { closeAlertHandler }>
+                          <Alert onClose={closeAlertHandler} severity="success">
+                            {approveInvite} 
+                          </Alert>
+        </Snackbar>
     </div>
   );
 }
