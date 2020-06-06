30be3062916cf31661e501100701ab1c93361e36
Carol Chung <cch5ng@gmail.com>
2020-06-05 18:09:11 -0700

    add test file for translate api

---
+++ server/util/google_translate_test.js
@@ -0,0 +1,25 @@
+const {Translate} = require('@google-cloud/translate').v2;
+const GOOGLE_APPLICATION_CREDENTIALS = require('../world-messenger-a751eb6a9d4f.json');
+
+const {project_id} = GOOGLE_APPLICATION_CREDENTIALS;
+
+//const projectId = project_id
+const keyFilename = '../world-messenger-a751eb6a9d4f.json';
+
+// // Instantiates a client
+const translate = new Translate({projectId: project_id, keyFilename});
+
+async function quickStart() {
+  // The text to translate
+  const text = 'Hello, world!';
+
+  // The target language
+  const target = 'ru';
+
+  // Translates some text into Russian
+  const [translation] = await translate.translate(text, target);
+  console.log(`Text: ${text}`);
+  console.log(`Translation: ${translation}`);
+}
+
+quickStart();
