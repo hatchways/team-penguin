f8405a93744be55313894c503bbdf0d4bcd20d02
Carol Chung <cch5ng@gmail.com>
2020-06-08 13:39:18 -0700

    refactor languages to more easily get array of languages for conversation

--- server/util/languages.js
+++ server/util/languages.js
@@ -1,9 +1,13 @@
+/*
+  use this store as kind of memoization; when the user initiates a conversation, if the languages data is not stored yet, add it here bc want to avoid having to make a query on each message in the conversation and the user-language data is not getting passed in the contacts list currently
+*/
 //indexed by conversation_id
 let languages = {
-  '5ede5f05e00aa60562b1d9c5': {
-    'test90@t.com': 'english',
-    'test170@t.com': 'spanish'
-  }
+  '5ede9f01091edb1b37240a33': ['english', 'spanish']
+  //{
+    //'test90@t.com': 'english',
+    //'test180@t.com': 'spanish'
+  //}
 }
 
 //counting mandarin as simplified chinese
