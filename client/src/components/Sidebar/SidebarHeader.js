fb6f70f3dca17ebb2b83c69eecaa99946ec0d36c
Rajat Bansal <rjtbansal@gmail.com>
2020-06-08 16:27:47 -0400

    added logout to menu item to horizontal more icon. More styling needed

--- client/src/components/Sidebar/SidebarHeader.js
+++ client/src/components/Sidebar/SidebarHeader.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { deepOrange, deepPurple } from '@material-ui/core/colors';
-
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 import {useAuth} from '../../context/auth-context';
 import './style.css'
 
@@ -26,6 +31,9 @@ const useStyles = makeStyles((theme) => ({
   chatHeaderName: {
     padding: '0 5px',
   },
+  paper: {
+    marginRight: theme.spacing(2),
+  }
 }));
 
 const SidebarHeader = (props) => {
@@ -33,8 +41,31 @@ const SidebarHeader = (props) => {
   const email = user.email;
   const initial = user.email[0].toUpperCase();
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const anchorRef = React.useRef(null);
 
-  const handleLogout = (evt) => {
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+
+  const handleLogout = () => {
     logout();
   }
 
@@ -47,8 +78,28 @@ const SidebarHeader = (props) => {
       </div>
       <div className="headerSpacer" />
       <div className="headerRight">
-        <Icon onClick={handleLogout}>more_horiz</Icon>
-      </div>
+        <MoreHorizIcon 
+          ref={anchorRef}
+          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle} />
+          <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow">
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+        </div>
     </div>
   )
 }
