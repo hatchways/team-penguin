1e1feaf684ff9262fdb7fc3ff64f0455a9c99a43
Carol Chung <cch5ng@gmail.com>
2020-06-08 14:22:24 -0700

    make the getTranslation() params more like the working original

--- server/util/translate_helpers.js
+++ server/util/translate_helpers.js
@@ -2,12 +2,12 @@ const {Translate} = require('@google-cloud/translate').v2;
 const GOOGLE_APPLICATION_CREDENTIALS = require('../world-messenger-a751eb6a9d4f.json');
 
 const {project_id} = GOOGLE_APPLICATION_CREDENTIALS;
-const keyFilename = '../world-messenger-a751eb6a9d4f.json';
+const keyFilename = '../server/world-messenger-a751eb6a9d4f.json';
 
 const translate = new Translate({projectId: project_id, keyFilename});
 
-async function getTranslation({language_code, message}) {
-  const [translation] = await translate.translate(message, language_code);
+async function getTranslation(text, target) {
+  const [translation] = await translate.translate(text, target);
   console.log(`Text: ${text}`);
   console.log(`Translation: ${translation}`);
   return translation;
