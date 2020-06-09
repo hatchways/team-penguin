5fb77f9c376a2d618d50b4d18df078d9e47cf071
Carol Chung <cch5ng@gmail.com>
2020-06-09 12:44:37 -0700

    try to scroll to last item in list; not working

--- client/src/components/Chat/Message.js
+++ client/src/components/Chat/Message.js
@@ -9,7 +9,7 @@ import {getPrettyTime} from '../../util/helpers';
 import {useAuth} from '../../context/auth-context';
 
 const Message = props => {
-  const {message, userEmail, messageTime} = props;
+  const {message, userEmail, messageTime, ref} = props;
   const {user} = useAuth();
   const {language} = user;
   var msgClass = classNames({
@@ -29,7 +29,7 @@ const Message = props => {
     const translation = message.translations ? message.translations[language] : '';
 
     return (
-      <div style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'flex-start', alignItems: 'flex-start', margin: '18px 0'}}>
+      <div ref={ref} style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'flex-start', alignItems: 'flex-start', margin: '18px 0'}}>
         <Avatar>{getUserInitial()}</Avatar>
         <div style={{display: 'flex', flexFlow: 'column nowrap', marginLeft: '8px'}}>
           <Typography variant='body1' className="messageAuthorTime">{userEmail} {getPrettyTime(messageTime)}</Typography>
@@ -39,7 +39,7 @@ const Message = props => {
     )  
   } else {
     return (
-      <div style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'flex-end', alignItems: 'flex-end', margin: '18px 0'}}>
+      <div ref={ref} style={{display: 'flex', flexFlow: 'row nowrap', justifyContent: 'flex-end', alignItems: 'flex-end', margin: '18px 0'}}>
         <div style={{display: 'flex', flexFlow: 'column nowrap', marginLeft: '8px', alignItems: 'flex-end'}}>
           <Typography variant='body1' className="messageAuthorTime">{getPrettyTime(messageTime)}</Typography>
           <div className={msgClass}>{props.message.original_message}</div>
--- client/src/components/Chat/MessageDisplay.js
+++ client/src/components/Chat/MessageDisplay.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import Message from './Message';
@@ -6,14 +6,33 @@ import {useAuth} from '../../context/auth-context';
 
 const MessageDisplay = props => {
   const {user} = useAuth();
+  const {messages} = props;
 
-  const messages = props.messages.map((msg, idx) => (
+  // console.log('messages', messages);
+  // const refs = messages.reduce((acc, value) => {
+  //   acc[value._id] = React.createRef();
+  //   return acc;
+  // }, {});
+
+  const scrollToEnd = () => {
+      let lastIdx = messages.length - 1;
+      let lastMsg = document.querySelector(`div[ref=${lastIdx}]`);
+  //   const lastIdx = messages.length - 1;
+      lastMsg.scrollIntoView({block: 'end', behavior: 'smooth'});
+  }
+
+  // useEffect(() => {
+  //   scrollToEnd();
+  // }, []);
+
+  const messageList = messages.map((msg, idx) => (
     <Message
       message={msg}
       messageTime={msg.created_on}
       userEmail={msg.author_email}
       key={`${idx}-${msg}`}
       isAuthorUser={msg.author_email === user.email}
+      ref={idx}
     />
   ))
 
@@ -21,7 +40,7 @@ const MessageDisplay = props => {
     <div
       style={{backgroundColor: '#fff', padding: '18px', 
       overflow: 'scroll', flexGrow: '1'}}>
-      {messages}
+      {messageList}
     </div>
 )};
 
