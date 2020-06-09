10cff73d55ac78348bffa2314a50e89d85cf6e63
Carol Chung <cch5ng@gmail.com>
2020-06-08 18:58:00 -0700

    removed guid for message; not used

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import { useParams, Link } from "react-router-dom";
-import { v4 as uuidv4 } from 'uuid';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -54,7 +53,6 @@ const Chat = props => {
     if (e.key === 'Enter') {
       e.preventDefault();
       let message = {
-        id: uuidv4(),
         author_id: user.id,
         author_email: user.email,
         original_message: curMessage,
