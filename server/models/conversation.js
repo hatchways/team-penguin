6944bcc64c93a1f18db7e353e1030d5841ec68a1
Carol Chung <cch5ng@gmail.com>
2020-06-11 12:08:36 -0700

    update conversation message to store guid

--- server/models/conversation.js
+++ server/models/conversation.js
@@ -11,6 +11,7 @@ var conversationSchema = new Schema({
       required: true
     },
     messages: [{
+      guid: String,
       author_id: {
         type: mongoose.Types.ObjectId,
         ref: 'users'
