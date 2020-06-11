5b2be5d873f799a67bb0ccecc91a019c38fa21c5
Carol Chung <cch5ng@gmail.com>
2020-06-10 18:43:05 -0700

    cleanup

--- client/src/components/Sidebar/SidebarHeader.js
+++ client/src/components/Sidebar/SidebarHeader.js
@@ -80,7 +80,6 @@ const SidebarHeader = (props) => {
       <div className="headerLeft">
         <Avatar className={classes.purple}>{initial}</Avatar>
         <Typography variant='h5' className={classes.chatHeaderName}>{email.split('@')[0]}</Typography>
-        <Typography variant='p2'>online status todo</Typography>
       </div>
       <div className="headerSpacer" />
       <div className="headerRight">
